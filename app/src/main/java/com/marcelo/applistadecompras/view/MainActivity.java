be24683fe66d7424c052a0e9aaa079e53645792b
Marcelo Santos <rocha.mer21@gmail.com>
2021-10-13 16:27:37 -0300

    fix em alguns componentes

--- app/src/main/java/com/marcelo/applistadecompras/view/MainActivity.java
+++ app/src/main/java/com/marcelo/applistadecompras/view/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_sair) {
             return true;
         }
 
